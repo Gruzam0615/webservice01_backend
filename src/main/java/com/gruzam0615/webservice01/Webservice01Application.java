9d7f4cc6c9ebfb1ea5fe2f404f1959a42ae79f19
gruzam0615 <gruzam0615@gmail.com>
2024-05-03 00:31:11 +0900

    Project init

---
+++ src/main/java/com/gruzam0615/webservice01/Webservice01Application.java
@@ -0,0 +1,13 @@
+package com.gruzam0615.webservice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webservice01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruzam0615/webservice01/Webservice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzam0615.webservice01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webservice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
