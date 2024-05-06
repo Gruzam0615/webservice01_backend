8f5cf053d0eacd9690f03433c852180a34d3d31f
gruzam0615 <gruzam0615@gmail.com>
2024-05-07 06:48:13 +0900

    RollBack to Init

--- src/main/java/com/gruzam0615/webservice01/utils/handler/ResponseHandler.java
+++
@@ -1,9 +0,0 @@
-package com.gruzam0615.webservice01.utils.handler;
-
-import org.springframework.http.HttpStatus;
-
-import lombok.Builder;
-
-@Builder
-public record ResponseHandler<T>(HttpStatus status, String message, T data) {
-}
--- src/main/java/com/gruzam0615/webservice01/utils/security/jwt/JwtService.java
+++
@@ -1,8 +0,0 @@
-package com.gruzam0615.webservice01.utils.security.jwt;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class JwtService {
-    
-}
